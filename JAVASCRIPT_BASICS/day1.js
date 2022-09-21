661fe25d2c62ea8beb5f74bad78fe79806c6f699
Amar Kumar <87652230+Amar8052@users.noreply.github.com>
2022-09-22 02:42:26 +0530

    js 1st_Day

--- JAVASCRIPT_BASICS/day1.js
+++ JAVASCRIPT_BASICS/day1.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const colors=["red","green","pink","yello","orange","black"];
+const btn= document.getElementById('btn');
+const color = document.querySelector(".color");
+console.log(randomNumber)
+btn.addEventListener('click',function(){
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = color[randomNumber]
+})
+function getRandomNumber(){
+    return Math.random();
+}
+<script>
+    alert('hello')
+</script>
